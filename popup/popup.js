23f838451fe8cc3d149a1ae6be1a9de342c2fb16
usy <contact.usyless@gmail.com>
2024-10-15 21:33:29 +0100

    simplify popup, fix popup error

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,5 @@
-assign_button_to_page(document.getElementById("reversing"), "reverse_page/image_to_source.html");
-assign_button_to_page(document.getElementById("settings"), "settings/settings.html");
-
-function assign_button_to_page(button, page_path) {
-    button.addEventListener('click', () => {
-        chrome.tabs.create({url: chrome.runtime.getURL(page_path)});
+document.addEventListener("click", (e) => {
+    if (e.target.nodeName === "A") setTimeout(() => {
         window.close();
-    })
-}
+    }, 10);
+});
