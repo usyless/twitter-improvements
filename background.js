679e59dffe450ff3448e5c9457b79394ce10a7fb
usy <contact.usyless@gmail.com>
2024-10-17 23:12:23 +0100

    slightly more redundant video downloading

--- background.js
+++ background.js
@@ -61,7 +61,7 @@ function getVideoFileType(url) {
     else return ".gif";
 }
 
-const getBestQuality = (variants) => variants.filter(v => v.content_type === "video/mp4").reduce((x, y) => Number(x?.bitrate) > Number(y?.bitrate) ? x : y).url;
+const getBestQuality = (variants) => variants.filter(v => v?.content_type === "video/mp4").reduce((x, y) => Number(x?.bitrate) > Number(y?.bitrate) ? x : y).url;
 const tweetDetails = 'https://x.com/i/api/graphql/nBS-WpgA6ZG0CyNHD517JQ/TweetDetail';
 const features = JSON.stringify({"rweb_tipjar_consumption_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"rweb_video_timestamps_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_enhance_cards_enabled":false});
 const fieldToggles = JSON.stringify({"withArticleRichContentState":true,"withArticlePlainText":false,"withGrokAnalyze":false,"withDisallowedReplyControls":false});
@@ -72,22 +72,23 @@ const defaultHeaders = {
     'content-type': 'application/json', 'X-Twitter-Auth-Type': 'OAuth2Session',
 }
 async function download_cobalt(request, sendResponse) {
-    const filename = getFileName(request.url), id = request.url.split("/").slice(-1)[0];
     try {
         if (/Android/i.test(navigator.userAgent)) throw new Error("android"); // open in new tab if on android
-        const tweetDetailsURL = new URL(tweetDetails);
+        const filename = getFileName(request.url), id = request.url.split("/").slice(-1)[0],
+            tweetDetailsURL = new URL(tweetDetails);
         tweetDetailsURL.searchParams.set('variables', JSON.stringify({"focalTweetId": id, ...variables}));
         tweetDetailsURL.searchParams.set('features', features);
         tweetDetailsURL.searchParams.set('fieldToggles', fieldToggles);
         const headers = {'user-agent': navigator.userAgent, 'x-csrf-token': request.cookie, ...defaultHeaders}; // Cookie sent by browser so no need to set myself
-        (await (await fetch(tweetDetailsURL, { headers })).json())?.data
+        const urls = (await (await fetch(tweetDetailsURL, { headers })).json())?.data
             ?.threaded_conversation_with_injections_v2?.instructions?.find(a => a?.type === "TimelineAddEntries")
             ?.entries?.find(a => a?.entryId.includes(id))?.content?.itemContent?.tweet_results?.result?.legacy
             ?.entities?.media?.filter(m => ["video", "animated_gif"].includes(m?.type))
-            ?.map(m => getBestQuality(m?.video_info?.variants))?.forEach((url, i) => {
-                download(url, `${filename}${i + 1}${getVideoFileType(url)}`);
-        });
-        sendResponse({status: 'success'});
+            ?.map(m => getBestQuality(m?.video_info?.variants));
+        if (urls?.length > 0) {
+            urls.forEach((url, i) => download(url, `${filename}${i + 1}${getVideoFileType(url)}`));
+            sendResponse({status: 'success'});
+        } else throw new Error("no videos");
     } catch {
         if (request.video_download_fallback) {
             sendResponse({status: 'newpage', copy: filename});
