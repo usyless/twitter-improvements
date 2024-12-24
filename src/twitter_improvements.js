27463d5a3841e3c89f09641d069786b72bfe8c86
usy <contact.usyless@gmail.com>
2024-12-24 13:06:01 +0000

    dont keep any video downloading details by default

--- src/twitter_improvements.js
+++ src/twitter_improvements.js
@@ -372,7 +372,7 @@
             Notification.create(`Saving Image${Settings.about.android ? ' (This may take a second on android)' : ''}`);
         } else if (message.type === 'downloadDetails') {
             let changeMade = false;
-            for (const n in message) if (Settings.videoDownloading[n] && Settings.videoDownloading[n] !== message[n]) {
+            for (const n in message) if (Settings.videoDownloading[n] != null && Settings.videoDownloading[n] !== message[n]) {
                 Settings.videoDownloading[n] = message[n];
                 changeMade = true;
             }
@@ -419,10 +419,10 @@
             }
 
             videoDownloading = {
-                detailsURL: 'https://x.com/i/api/graphql/nBS-WpgA6ZG0CyNHD517JQ/TweetDetail',
-                authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
-                features: '{"responsive_web_live_screen_enabled":false,"rweb_tipjar_consumption_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"rweb_video_timestamps_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_enhance_cards_enabled":false}',
-                fieldToggles: '{"withArticleRichContentState":true,"withArticlePlainText":false,"withGrokAnalyze":false,"withDisallowedReplyControls":false}'
+                detailsURL: '',
+                authorization: '',
+                features: '',
+                fieldToggles: ''
             }
 
             about = {
