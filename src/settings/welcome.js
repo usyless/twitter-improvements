a1a1eac6ca2c612e9318354b74f8b5129a2c550b
usy <contact.usyless@gmail.com>
2025-01-05 13:54:43 +0000

    scroll window on drag

--- src/settings/welcome.js
+++ src/settings/welcome.js
@@ -50,9 +50,17 @@ if ((new URLSearchParams(window.location.search)).get('installed')) {
 
 
     window.addEventListener('pointerdown', (e) => {
+        const moveController = new AbortController();
         const onExit = (ev) => {
+            moveController.abort();
+            overlay.style.transition = '';
             if (e.clientY - ev.clientY > 150) finishIntroduction();
+            else overlay.style.marginTop = '';
         };
+        overlay.style.transition = '0s';
+        window.addEventListener('pointermove', (ev) => {
+            overlay.style.marginTop = `-${Math.max(e.clientY - ev.clientY, 0)}px`;
+        }, {signal: moveController.signal});
         window.addEventListener('pointerup', onExit, {once: true, signal: controller.signal});
         window.addEventListener('pointercancel', onExit, {once: true, signal: controller.signal});
     }, {signal: controller.signal});
