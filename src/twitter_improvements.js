209c45ccf79f31795a665f7302dacf15eabbb02d
usy <contact.usyless@gmail.com>
2025-02-12 13:44:33 +0000

    implement button scale

--- src/twitter_improvements.js
+++ src/twitter_improvements.js
@@ -196,6 +196,7 @@
                 button = Button.newButton(Tweet.anchorWithFallback(Tweet.nearestTweet(image)), download_button_path, (e) => Image.imageButtonCallback(e, image), "usy-image", (e) => Image.removeImageDownloadCallback(e, image));
                 button.style.width = Settings.image_preferences.long_image_button ? '100%' : 'fit-content';
                 button.classList.add(...(Image.buttonModes[Settings.image_preferences.image_button_position] ?? []));
+                button.style.transform = `scale(${Settings.image_preferences.image_button_scale})`;
 
                 image.after(button);
                 if (Settings.image_preferences.download_history_enabled) { // mark image
