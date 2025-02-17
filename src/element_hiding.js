f66a7906b26255858f16b00df583f12bb64d9233
usy <contact.usyless@gmail.com>
2025-02-17 18:06:59 +0000

    rename variables

--- src/element_hiding.js
+++ src/element_hiding.js
@@ -7,13 +7,13 @@
 
     const loadSettings = () => browser.runtime.sendMessage({type: 'get_settings'});
 
-    const Styles = {
+    const HideType = {
         DISPLAY: '{display:none!important;}',
         VISIBILITY: '{visibility:hidden!important;pointer-events: none!important;}',
     }
 
     // Naming dependent on style.tweet_button_positions
-    const sharedSelectors = {
+    const CommonSelectors = {
         views: ['div:has(> a[href$="/analytics"])'],
         share: ['div:has(> div > button[aria-label="Share post"]:not([usy]))'],
         replies: ['div:has(> button[data-testid="reply"])'],
@@ -26,9 +26,9 @@
     }
 
     // Naming dependent on style
-    const StyleMap = {
-        hide_notifications: {s: ['a[href="/notifications"]'], st: Styles.DISPLAY},
-        hide_messages: {s: ['a[href="/messages"]'], st: Styles.DISPLAY},
+    const SelectorMap = {
+        hide_notifications: {s: ['a[href="/notifications"]'], st: HideType.DISPLAY},
+        hide_messages: {s: ['a[href="/messages"]'], st: HideType.DISPLAY},
         hide_grok: {s: [
             'a[href="/i/grok"]',
             'button[aria-label="Grok actions"]',
@@ -37,46 +37,46 @@
             'button[aria-label="Profile Summary"]',
             'div.css-175oi2r.r-1777fci.r-1wzrnnt',
             'div[data-testid="GrokDrawer"]'
-        ], st: Styles.DISPLAY},
-        hide_jobs: {s: ['a[href="/jobs"]'], st: Styles.DISPLAY},
-        hide_lists: {s: ['a[href*="/lists"]'], st: Styles.DISPLAY},
-        hide_communities: {s: ['a[href*="/communities"]'], st: Styles.DISPLAY},
+        ], st: HideType.DISPLAY},
+        hide_jobs: {s: ['a[href="/jobs"]'], st: HideType.DISPLAY},
+        hide_lists: {s: ['a[href*="/lists"]'], st: HideType.DISPLAY},
+        hide_communities: {s: ['a[href*="/communities"]'], st: HideType.DISPLAY},
         hide_premium: {s: [
             'a[href="/i/premium_sign_up"]',
             'aside[aria-label*="Premium"]',
             'div:has(> * > aside[aria-label*="Premium"])',
             'div:has(> * > aside[aria-label="Ending today!"])'
-        ], st: Styles.DISPLAY},
-        hide_verified_orgs: {s: ['a[href="/i/verified-orgs-signup"]'], st: Styles.DISPLAY},
+        ], st: HideType.DISPLAY},
+        hide_verified_orgs: {s: ['a[href="/i/verified-orgs-signup"]'], st: HideType.DISPLAY},
         hide_monetization: {s: [
             'a[href="/settings/monetization"]',
             'a[href="/i/monetization"]'
-        ], st: Styles.DISPLAY},
-        hide_ads_button: {s: ['a[href*="https://ads.x.com"]'], st: Styles.DISPLAY},
-        hide_whats_happening: {s: ['div:has(> * > [aria-label="Timeline: Trending now"])'], st: Styles.DISPLAY},
+        ], st: HideType.DISPLAY},
+        hide_ads_button: {s: ['a[href*="https://ads.x.com"]'], st: HideType.DISPLAY},
+        hide_whats_happening: {s: ['div:has(> * > [aria-label="Timeline: Trending now"])'], st: HideType.DISPLAY},
         hide_who_to_follow: {s: [
             'div:has(> * > [aria-label="Who to follow"])',
             'div:has(> * > * > [aria-label="Loading recommendations for users to follow"])'
-        ], st: Styles.DISPLAY},
-        hide_relevant_people: {s: ['div:has(> [aria-label="Relevant people"])'], st: Styles.DISPLAY},
-        hide_create_your_space: {s: ['a[href="/i/spaces/start"]'], st: Styles.DISPLAY},
-        hide_post_button: {s: ['div:has(> a[href="/compose/post"])'], st: Styles.DISPLAY},
-        hide_follower_requests: {s: ['a[href="/follower_requests"]'], st: Styles.DISPLAY},
+        ], st: HideType.DISPLAY},
+        hide_relevant_people: {s: ['div:has(> [aria-label="Relevant people"])'], st: HideType.DISPLAY},
+        hide_create_your_space: {s: ['a[href="/i/spaces/start"]'], st: HideType.DISPLAY},
+        hide_post_button: {s: ['div:has(> a[href="/compose/post"])'], st: HideType.DISPLAY},
+        hide_follower_requests: {s: ['a[href="/follower_requests"]'], st: HideType.DISPLAY},
         hide_live_on_x: {s: [
             'div:has(> [data-testid="placementTracking"] > [aria-label^="Space,"])',
             'div:has(> [data-testid="placementTracking"] > [aria-label^="Broadcast, "])'
-        ], st: Styles.DISPLAY},
+        ], st: HideType.DISPLAY},
         hide_post_reply_sections: {s: [
             'div:has(> div > div[role="progressbar"] + div > div > div > div > div > div > div[data-testid^="UserAvatar-Container"])'
-        ], st: Styles.DISPLAY},
-        hide_sidebar_footer: {s: ['div:has(> [aria-label="Footer"])'], st: Styles.DISPLAY},
+        ], st: HideType.DISPLAY},
+        hide_sidebar_footer: {s: ['div:has(> [aria-label="Footer"])'], st: HideType.DISPLAY},
 
-        hide_tweet_view_count: {s: sharedSelectors.views, st: Styles.VISIBILITY},
-        hide_tweet_share_button: {s: sharedSelectors.share, st: Styles.VISIBILITY},
-        hide_replies_button_tweet: {s: sharedSelectors.replies, st: Styles.VISIBILITY},
-        hide_retweet_button_tweet: {s: sharedSelectors.retweets, st: Styles.VISIBILITY},
-        hide_like_button_tweet: {s: sharedSelectors.likes, st: Styles.VISIBILITY},
-        hide_bookmark_button_tweet: {s: sharedSelectors.bookmark, st: Styles.VISIBILITY}
+        hide_tweet_view_count: {s: CommonSelectors.views, st: HideType.VISIBILITY},
+        hide_tweet_share_button: {s: CommonSelectors.share, st: HideType.VISIBILITY},
+        hide_replies_button_tweet: {s: CommonSelectors.replies, st: HideType.VISIBILITY},
+        hide_retweet_button_tweet: {s: CommonSelectors.retweets, st: HideType.VISIBILITY},
+        hide_like_button_tweet: {s: CommonSelectors.likes, st: HideType.VISIBILITY},
+        hide_bookmark_button_tweet: {s: CommonSelectors.bookmark, st: HideType.VISIBILITY}
     }
 
     const start = () => loadSettings().then((settings) => {
@@ -87,14 +87,14 @@
         // Apply new enabled styles
         let style = '';
         for (const setting in enabled) if (enabled[setting] === true) {
-            const st = StyleMap?.[setting], styler = st.st;
+            const st = SelectorMap?.[setting], styler = st.st;
             for (const s of st.s) style += s + styler;
         }
 
         // Apply button ordering if exists
         const result = enabled.tweet_button_positions.match(/{([^}]+)}/g).map(match => match.replace(/[{}]/g, ''));
         for (let i = 0; i < result.length; ++i) {
-            for (const s of sharedSelectors[result[i]] ?? []) style += `${s}{order:${i}!important}`;
+            for (const s of CommonSelectors[result[i]] ?? []) style += `${s}{order:${i}!important}`;
         }
 
         if (style.length > 0) {
