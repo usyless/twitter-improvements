a4a5fe4f6ebe9c9920750482bcb34f1c6677b99f
usy <contact.usyless@gmail.com>
2025-04-28 01:09:22 +0100

    parse all media

--- src/fetch-intercept.js
+++ src/fetch-intercept.js
@@ -1,4 +1,13 @@
 (() =>  {
+    const getBestQuality = (variants) => variants.filter(v => v?.content_type === "video/mp4").reduce((x, y) => +x?.bitrate > +y?.bitrate ? x : y).url;
+    const postMedia = (media) => {
+        window.postMessage({
+            source: "ift",
+            type: "media-urls",
+            media
+        }, "https://x.com");
+    }
+
     const originalSend = XMLHttpRequest.prototype.send;
     XMLHttpRequest.prototype.send = function (body) {
         const xhr = this;
@@ -6,44 +15,59 @@
         this.addEventListener("readystatechange",  () => {
             if (xhr.readyState === 4) {
                 const responseURL = xhr.responseURL;
-                if (responseURL.includes('TweetDetail') && xhr.status === 200) {
-                    const json = JSON.parse(xhr.responseText),
-                        id = JSON.parse(new URL(decodeURIComponent(responseURL)).searchParams.get('variables')).focalTweetId;
-
-                    // use this for more than videos later on, fix scroll on the fullscreen images
-
-                    let urls = json?.data?.threaded_conversation_with_injections_v2?.instructions
-                        ?.find?.(a => a?.type === "TimelineAddEntries")?.entries?.find?.(a => a?.entryId?.includes?.(id))?.content
-                        ?.itemContent?.tweet_results?.result;
-                    urls = urls?.tweet ?? urls;
-                    urls = urls?.legacy?.entities?.media?.filter?.(m => ["video", "animated_gif"].includes?.(m?.type))
-                        ?.map?.(m => getBestQuality(m?.video_info?.variants));
-
-                    if (urls?.length === 0) {
-                        console.log("Attempting to brute force video download");
-                        urls = []
-                        for (const tweet of findAllPotentialTweetsById(json, id)) {
-                            const videos = findVideos(tweet);
-                            for (const key in videos) urls.push(videos[key].url);
-                        }
-                    }
-
-                    if (urls?.length > 0) {
-                        // we have the videos in the tweet
-                        window.postMessage({
-                            source: "ift",
-                            type: "media-urls",
-                            id,
-                            urls
-                        }, "https://x.com");
+                if (xhr.status === 200) {
+                    if (responseURL.includes('TweetDetail')) {
+                        // maximised tweet
+                        let media = mediaFromInstructions(JSON.parse(xhr.responseText)?.data
+                            ?.threaded_conversation_with_injections_v2?.instructions);
+                        if (media.length > 0) postMedia(media);
+                    } else if (responseURL.includes('HomeTimeline')) {
+                        // tweets at home
+                        let media = mediaFromInstructions(JSON.parse(xhr.responseText)?.data?.home?.home_timeline_urt
+                            ?.instructions);
+                        if (media.length > 0) postMedia(media);
                     }
                 }
             }
         });
         return originalSend.call(this, body);
     };
-    const getBestQuality = (variants) => variants.filter(v => v?.content_type === "video/mp4").reduce((x, y) => +x?.bitrate > +y?.bitrate ? x : y).url;
 
+    function mediaFromInstructions(instructions) {
+        return instructions
+            ?.find?.(a => a?.type === "TimelineAddEntries")?.entries
+            ?.filter(a => a?.entryId?.startsWith('tweet-'))?.map(getMediaInfo)
+            ?.filter(a => a != null);
+    }
+
+    function getMediaInfo(tweet) {
+        tweet = tweet?.content?.itemContent?.tweet_results?.result;
+        tweet = tweet?.tweet ?? tweet;
+        if (tweet) {
+            const id = tweet.rest_id;
+            tweet = tweet.legacy?.entities?.media;
+            if (id && tweet) {
+                // has media
+                const mediaInfo = [];
+                for (let i = 1; i <= tweet.length; ++i) {
+                    const media = tweet[i], info = {id: `${id}-${i}`};
+                    switch (media.type) {
+                        case 'photo': {
+                            const lastDot = media.media_url_https?.lastIndexOf('.');
+                            info.url = `${media.media_url_https?.substring(0, lastDot)}?format=${media.media_url_https?.substring(lastDot + 1)}&name=orig`;
+                            break;
+                        }
+                        case 'video': case 'animated_gif': {
+                            info.url = getBestQuality(media.video_info?.variants);
+                            break;
+                        }
+                    }
+                    mediaInfo.push(info);
+                }
+                return mediaInfo;
+            }
+        }
+    }
 
     function findAllPotentialTweetsById(data, id, results=[]) {
         if (Array.isArray(data)) for (const item of data) findAllPotentialTweetsById(item, id, results);
