0a46f62d2f8f19470825ad03094c5d0c81827e79
usy <contact.usyless@gmail.com>
2025-04-29 17:22:41 +0100

    update migration system

--- src/background.js
+++ src/background.js
@@ -284,157 +284,149 @@ function saveImage(url, sourceURL) {
     })
 }
 
+const migrations = {
+    '1.4': () => new Promise((resolve) => {
+        browser.storage.local.get().then(async (s) => {
+            if (s?.setting?.image_button != null) {
+                s.setting.media_download_button = s.setting.image_button;
+            }
+            if (s?.setting?.inline_image_button != null || s?.setting?.video_button != null) {
+                s.setting.inline_download_button = s?.setting?.inline_image_button || s?.setting?.video_button;
+            }
+
+            await browser.storage.local.set(s);
+
+            resolve();
+        });
+    }),
+    '1.2.1.5': () => new Promise((resolve) => {
+        browser.storage.local.get().then(async (s) => {
+            if (s?.image_preferences?.long_image_button != null) {
+                if (s.image_preferences.long_image_button === true) {
+                    s.image_preferences.image_button_width_value = '100';
+                }
+                delete s.image_preferences.long_image_button;
+                await browser.storage.local.set(s);
+            }
+
+            resolve();
+        });
+    }),
+    '1.1.1.4': () => new Promise((resolve) => {
+        getHistoryDB().then((db) => {
+            browser.storage.local.get(['download_history']).then(async (r) => {
+                const history = r.download_history ?? {};
+                const transaction = db.transaction(['download_history'], 'readwrite');
+                if (Object.keys(history).length > 0) {
+                    const objectStore = transaction.objectStore('download_history');
+                    for (const saved_image in history) objectStore.put({saved_image});
+                }
+
+                await browser.storage.local.remove('download_history');
+
+                transaction.addEventListener('complete', resolve);
+            });
+        });
+    }),
+    '1.1.1.1': () => new Promise((resolve) => {
+        browser.storage.local.get().then(async (s) => {
+            const {
+                // styles
+                hide_notifications, hide_messages, hide_grok, hide_jobs, hide_lists, hide_communities,
+                hide_premium, hide_verified_orgs, hide_monetization, hide_ads_button, hide_whats_happening,
+                hide_who_to_follow, hide_relevant_people, hide_create_your_space, hide_post_button,
+                hide_follower_requests, hide_live_on_x, hide_post_reply_sections, hide_sidebar_footer,
+                // settings
+                vx_button, video_button, image_button, show_hidden,
+                // preferences
+                url_prefix, video_download_fallback, long_image_button, custom_url, download_history_enabled,
+                download_history_prevent_download, download_history,
+                // videoDownloading
+                detailsURL, authorization, features, fieldToggles
+            } = s;
+
+            await browser.storage.local.clear();
+            const newSettings = {style: {}, setting: {}, vx_preferences: {}, video_details: {},
+                image_preferences: {}, video_preferences: {}};
+
+            if (hide_notifications != null) newSettings.style.hide_notifications = hide_notifications;
+            if (hide_messages != null) newSettings.style.hide_messages = hide_messages;
+            if (hide_grok != null) newSettings.style.hide_grok = hide_grok;
+            if (hide_jobs != null) newSettings.style.hide_jobs = hide_jobs;
+            if (hide_lists != null) newSettings.style.hide_lists = hide_lists;
+            if (hide_communities != null) newSettings.style.hide_communities = hide_communities;
+            if (hide_premium != null) newSettings.style.hide_premium = hide_premium;
+            if (hide_verified_orgs != null) newSettings.style.hide_verified_orgs = hide_verified_orgs;
+            if (hide_monetization != null) newSettings.style.hide_monetization = hide_monetization;
+            if (hide_ads_button != null) newSettings.style.hide_ads_button = hide_ads_button;
+            if (hide_whats_happening != null) newSettings.style.hide_whats_happening = hide_whats_happening;
+            if (hide_who_to_follow != null) newSettings.style.hide_who_to_follow = hide_who_to_follow;
+            if (hide_relevant_people != null) newSettings.style.hide_relevant_people = hide_relevant_people;
+            if (hide_create_your_space != null) newSettings.style.hide_create_your_space = hide_create_your_space;
+            if (hide_post_button != null) newSettings.style.hide_post_button = hide_post_button;
+            if (hide_follower_requests != null) newSettings.style.hide_follower_requests = hide_follower_requests;
+            if (hide_live_on_x != null) newSettings.style.hide_live_on_x = hide_live_on_x;
+            if (hide_post_reply_sections != null) newSettings.style.hide_post_reply_sections = hide_post_reply_sections;
+            if (hide_sidebar_footer != null) newSettings.style.hide_sidebar_footer = hide_sidebar_footer;
+
+            if (vx_button != null) newSettings.setting.vx_button = vx_button;
+            if (video_button != null) newSettings.setting.video_button = video_button;
+            if (image_button != null) newSettings.setting.image_button = image_button;
+            if (show_hidden != null) newSettings.setting.show_hidden = show_hidden;
+
+            if (url_prefix != null) newSettings.vx_preferences.url_prefix = url_prefix;
+            if (custom_url != null) newSettings.vx_preferences.custom_url = custom_url;
+
+            if (download_history_prevent_download != null) newSettings.image_preferences.download_history_prevent_download = download_history_prevent_download;
+            if (long_image_button != null) newSettings.image_preferences.long_image_button = long_image_button;
+            if (download_history_enabled != null) newSettings.image_preferences.download_history_enabled = download_history_enabled;
+
+            if (video_download_fallback != null) newSettings.video_preferences.video_download_fallback = video_download_fallback;
+
+            if (detailsURL != null) newSettings.video_details.detailsURL = detailsURL;
+            if (authorization != null) newSettings.video_details.authorization = authorization;
+            if (features != null) newSettings.video_details.features = features;
+            if (fieldToggles != null) newSettings.video_details.fieldToggles = fieldToggles;
+
+            if (download_history != null) newSettings.download_history = download_history;
+
+            await browser.storage.local.set(newSettings);
+
+            resolve();
+        });
+    }),
+    '1.0.7.3': () => new Promise((resolve) => {
+        browser.storage.local.get(['url_prefix']).then(async (s) => {
+            if (s.url_prefix === 'vx') await browser.storage.local.set({url_prefix: 'fixvx.com'});
+            if (s.url_prefix === 'fx') await browser.storage.local.set({url_prefix: 'fixupx.com'});
+            resolve();
+        });
+    })
+}
+
 // migrating to new settings format
+/**
+ * @param {string} previousVersion
+ * @returns {Promise<void>}
+ */
 async function migrateSettings(previousVersion) {
+    previousVersion = (previousVersion?.match?.(/\d+/g) ?? []).join('');
     /**
      * @param {string} maxVersion
      * @returns {boolean}
      */
-    const versionBelowGiven = (maxVersion) => {
-        previousVersion = (previousVersion?.match?.(/\d+/g) ?? []).join('');
+    const previousIsBelow = (maxVersion) => {
         maxVersion = (maxVersion?.match?.(/\d+/g) ?? []).join('');
         const length = Math.max(previousVersion.length, maxVersion.length);
         return +previousVersion.padEnd(length, '0') < +maxVersion.padEnd(length, '0');
     }
 
-    // Fix for old link copying setting
-    if (versionBelowGiven('1.0.7.3')) {
-        console.log("Migrating vx and fx settings to new format");
-        browser.storage.local.get(['url_prefix']).then(async (s) => {
-            if (s.url_prefix === 'vx') await browser.storage.local.set({url_prefix: 'fixvx.com'});
-            if (s.url_prefix === 'fx') await browser.storage.local.set({url_prefix: 'fixupx.com'});
-        });
+    const m = [];
+    for (const version in migrations) {
+        if (previousIsBelow(version)) m.push(migrations[version]);
+        else break;
     }
-
-    // 1.1.1.1 is settings migrate update
-    if (versionBelowGiven('1.1.1.1')) {
-        console.log("Migrating settings to new format");
-        await new Promise((resolve) => {
-            browser.storage.local.get().then(async (s) => {
-                const {
-                    // styles
-                    hide_notifications, hide_messages, hide_grok, hide_jobs, hide_lists, hide_communities,
-                    hide_premium, hide_verified_orgs, hide_monetization, hide_ads_button, hide_whats_happening,
-                    hide_who_to_follow, hide_relevant_people, hide_create_your_space, hide_post_button,
-                    hide_follower_requests, hide_live_on_x, hide_post_reply_sections, hide_sidebar_footer,
-                    // settings
-                    vx_button, video_button, image_button, show_hidden,
-                    // preferences
-                    url_prefix, video_download_fallback, long_image_button, custom_url, download_history_enabled,
-                    download_history_prevent_download, download_history,
-                    // videoDownloading
-                    detailsURL, authorization, features, fieldToggles
-                } = s;
-
-                await browser.storage.local.clear();
-                const newSettings = {style: {}, setting: {}, vx_preferences: {}, video_details: {},
-                    image_preferences: {}, video_preferences: {}};
-
-                if (hide_notifications != null) newSettings.style.hide_notifications = hide_notifications;
-                if (hide_messages != null) newSettings.style.hide_messages = hide_messages;
-                if (hide_grok != null) newSettings.style.hide_grok = hide_grok;
-                if (hide_jobs != null) newSettings.style.hide_jobs = hide_jobs;
-                if (hide_lists != null) newSettings.style.hide_lists = hide_lists;
-                if (hide_communities != null) newSettings.style.hide_communities = hide_communities;
-                if (hide_premium != null) newSettings.style.hide_premium = hide_premium;
-                if (hide_verified_orgs != null) newSettings.style.hide_verified_orgs = hide_verified_orgs;
-                if (hide_monetization != null) newSettings.style.hide_monetization = hide_monetization;
-                if (hide_ads_button != null) newSettings.style.hide_ads_button = hide_ads_button;
-                if (hide_whats_happening != null) newSettings.style.hide_whats_happening = hide_whats_happening;
-                if (hide_who_to_follow != null) newSettings.style.hide_who_to_follow = hide_who_to_follow;
-                if (hide_relevant_people != null) newSettings.style.hide_relevant_people = hide_relevant_people;
-                if (hide_create_your_space != null) newSettings.style.hide_create_your_space = hide_create_your_space;
-                if (hide_post_button != null) newSettings.style.hide_post_button = hide_post_button;
-                if (hide_follower_requests != null) newSettings.style.hide_follower_requests = hide_follower_requests;
-                if (hide_live_on_x != null) newSettings.style.hide_live_on_x = hide_live_on_x;
-                if (hide_post_reply_sections != null) newSettings.style.hide_post_reply_sections = hide_post_reply_sections;
-                if (hide_sidebar_footer != null) newSettings.style.hide_sidebar_footer = hide_sidebar_footer;
-
-                if (vx_button != null) newSettings.setting.vx_button = vx_button;
-                if (video_button != null) newSettings.setting.video_button = video_button;
-                if (image_button != null) newSettings.setting.image_button = image_button;
-                if (show_hidden != null) newSettings.setting.show_hidden = show_hidden;
-
-                if (url_prefix != null) newSettings.vx_preferences.url_prefix = url_prefix;
-                if (custom_url != null) newSettings.vx_preferences.custom_url = custom_url;
-
-                if (download_history_prevent_download != null) newSettings.image_preferences.download_history_prevent_download = download_history_prevent_download;
-                if (long_image_button != null) newSettings.image_preferences.long_image_button = long_image_button;
-                if (download_history_enabled != null) newSettings.image_preferences.download_history_enabled = download_history_enabled;
-
-                if (video_download_fallback != null) newSettings.video_preferences.video_download_fallback = video_download_fallback;
-
-                if (detailsURL != null) newSettings.video_details.detailsURL = detailsURL;
-                if (authorization != null) newSettings.video_details.authorization = authorization;
-                if (features != null) newSettings.video_details.features = features;
-                if (fieldToggles != null) newSettings.video_details.fieldToggles = fieldToggles;
-
-                if (download_history != null) newSettings.download_history = download_history;
-
-                await browser.storage.local.set(newSettings);
-
-                resolve();
-            });
-        });
-    }
-
-    // migrate history to indexed db
-    if (versionBelowGiven('1.1.1.4')) {
-        console.log("Migrating history to indexed db");
-        await new Promise((resolve) => {
-            getHistoryDB().then((db) => {
-                browser.storage.local.get(['download_history']).then(async (r) => {
-                    const history = r.download_history ?? {};
-                    const transaction = db.transaction(['download_history'], 'readwrite');
-                    if (Object.keys(history).length > 0) {
-                        const objectStore = transaction.objectStore('download_history');
-                        for (const saved_image in history) objectStore.put({saved_image});
-                    }
-
-                    await browser.storage.local.remove('download_history');
-
-                    transaction.addEventListener('complete', resolve);
-                });
-            });
-        });
-    }
-
-    // remove long image button and replace with width setting
-    if (versionBelowGiven('1.2.1.5')) {
-        console.log("Removing and replacing long image button");
-        await new Promise((resolve) => {
-            browser.storage.local.get().then(async (s) => {
-                if (s?.image_preferences?.long_image_button != null) {
-                    if (s.image_preferences.long_image_button === true) {
-                        s.image_preferences.image_button_width_value = '100';
-                    }
-                    delete s.image_preferences.long_image_button;
-                    await browser.storage.local.set(s);
-                }
-
-                resolve();
-            });
-        })
-    }
-
-    // move to just inline or on media buttons
-    if (versionBelowGiven('1.4')) {
-        console.log("Replacing inline and media button settings");
-        await new Promise((resolve) => {
-            browser.storage.local.get().then(async (s) => {
-                if (s?.setting?.image_button != null) {
-                    s.setting.media_download_button = s.setting.image_button;
-                }
-                if (s?.setting?.inline_image_button != null || s?.setting?.video_button != null) {
-                    s.setting.inline_download_button = s?.setting?.inline_image_button || s?.setting?.video_button;
-                }
-
-                await browser.storage.local.set(s);
-
-                resolve();
-            });
-        });
-    }
+    for (let i = m.length - 1; i >= 0; --i) await m[i]();
 }
 
 function updateHistoryDb() {
