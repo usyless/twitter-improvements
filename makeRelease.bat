630909e14797b3e0328bf4a98a07ec534a13da53
usy <contact.usyless@gmail.com>
2025-05-09 17:07:53 +0100

    dont remove the jsdoc comments

--- makeRelease.bat
+++ makeRelease.bat
@@ -7,7 +7,6 @@ cd /d "%~dp0"
 :: Config file name
 set "releaseDirectory=releases\"
 set "srcDirectory=src\"
-set "buildDirectory=build\"
 
 :: Check 7z exists
 where 7z >nul 2>&1
@@ -32,38 +31,25 @@ set "chromium_zip=%currentFolder% chromium v%version%.zip"
 :: Add manifest.json to file list if not included already
 set "fileList=* manifest.json"
 
-:: remove build directory if exists, then copy src
-if exist "%buildDirectory%" (
-    rmdir /S /Q "%buildDirectory%"
-)
-xcopy /E /I /H /Y "%srcDirectory%" "%buildDirectory%" >nul 2>&1
-
-:: iterate through all files in build directory
-for /r "%buildDirectory%" %%f in (*.js) do (
-    :: remove jsdoc comments
-    powershell -Command "$content = Get-Content '%%f' -Raw; $content = $content -replace '(?s)/\*\*.*?\*/', ''; Set-Content '%%f' -Value $content"
-
-    echo Removed JSDoc comments from %%f
-)
-
-:: Change to build directory
-pushd %buildDirectory%
+:: switch to src directory
+pushd %srcDirectory%
 
 :: move chrome manifest outside
 move manifest_chrome.json ..
 7z a "..\%releaseDirectory%%firefox_zip%" %fileList%
 
-:: delete firefox manifest and move chrome one in
-del manifest.json
+:: move firefox manifest out and move chrome one in
+move manifest.json ..
 move ..\manifest_chrome.json manifest.json
 
 7z a "..\%releaseDirectory%%chromium_zip%" %fileList%
 
+:: rename chrome manifest and bring firefox one back in
+ren manifest.json manifest_chrome.json
+move ..\manifest.json .
+
 :: Return to original directory
 popd
-
-:: delete build directory
-rmdir /S /Q "%buildDirectory%"
 
 echo Built: %firefox_zip%, %chromium_zip%
 pause
