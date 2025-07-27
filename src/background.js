f12a27e5037c2f09376c994455d87d7f310abc56
usy <contact.usyless@gmail.com>
2025-07-27 03:34:51 +0100

    enable media page download picker by default

--- src/background.js
+++ src/background.js
@@ -110,7 +110,7 @@ const defaultSettings = {
         download_history_prevent_download: false,
 
         use_download_progress: false,
-        download_picker_on_media_page: false
+        download_picker_on_media_page: true
     },
 
     style: {
