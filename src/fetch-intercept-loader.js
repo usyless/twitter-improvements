1a72409838d55d41a937eda59f5894134cc59a48
usy <contact.usyless@gmail.com>
2026-03-27 18:35:31 +0000

    ensure no missed events, although extremely unlikely there would be one

--- src/fetch-intercept-loader.js
+++ src/fetch-intercept-loader.js
@@ -4,6 +4,23 @@
     script.src = chrome.runtime.getURL('/fetch-intercept.js');
     (document.head || document.documentElement).appendChild(script);
 
+    const queue = [];
+    const initialListener = (e) => {
+        e.stopPropagation();
+        if (e.detail) {
+            if (globalThis.ti_on_intercepted) {
+                if (queue.length > 0) {
+                    for (const data of queue) globalThis.ti_on_intercepted(data);
+                    queue.length = 0;
+                }
+                window.removeEventListener('ti-window-twt-data', initialListener, { capture: true });
+            } else {
+                queue.push(JSON.parse(e.detail));
+            }
+        }
+    };
+
+    window.addEventListener('ti-window-twt-data', initialListener, { capture: true });
     window.addEventListener('ti-window-twt-data', (e) => {
         e.stopPropagation();
         e.stopImmediatePropagation();
